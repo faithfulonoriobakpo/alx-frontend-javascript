1c2dfb6ae1feb8d16742c4e1a10f3770b4532f3c
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2022-12-22 22:41:07 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,4 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      /* eslint-disable */
-      if (true) {
-        resolve();
-      } else {
-        reject();
-      }
-      /* eslint-enable */
-    });
-  }
-export default getResponseFromAPI;
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
+}
