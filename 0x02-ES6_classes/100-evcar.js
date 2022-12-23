d978b3816175869fff2a3e7486101bef45b4610b
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2022-12-23 02:37:14 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,43 +1,15 @@
+import Car from './10-car';
 
-
-export default class Car {
-  constructor(brand, motor, color) {
-    this.brand = brand;
-    this.motor = motor;
-    this.color = color;
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(value) {
-    this._brand = value;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
-  }
-
-  static get [Symbol.species]() {
-    return this;
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-
-    return new Species();
+    const Obj = super.constructor || super[Symbol.species];
+    return new Obj();
   }
 }
+
+export default EVCar;
