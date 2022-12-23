1e16a5ce1ebc28797cec19a584ab0c9d06edc191
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2022-12-23 02:40:39 +0100

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
-class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
+export default class ClassRoom {
+	constructor (maxStudentsSize) {
+	this._maxStudentsSize = maxStudentsSize;
   }
 }
-export default Classroom;
