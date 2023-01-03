cb7c6bed99d38cf004812cfcbd15dccb13d1cc3a
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2023-01-03 12:44:27 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,4 @@
-export default function getStudentsByLocation(students, city) {
-    return students.filter((obj) => obj.location === city);
+export default function getStudentsByLocation(list, city) {
+  if (!Array.isArray(list) || typeof city !== 'string') return [];
+  return list.filter((value) => value.location === city);
 }
