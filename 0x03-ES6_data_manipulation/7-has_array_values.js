179ffe04b08de1a7344fb0d2c441fd0eadb36c6c
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2023-01-03 12:56:39 +0100

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-    return array.every((value) => set.has(value));
+  if (!set || !Array.isArray(array)) throw new Error('invalid parameter');
+  let output = true;
+  for (let i = 0; i < array.length; i += 1) {
+    output = set.has(array[i]);
+  }
+  return output;
 }
